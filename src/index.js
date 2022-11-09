bd15e2b292464dea5bd6bac93a7a9a284aac5efc
luis_antonio <tyluis20@gmail.com>
2022-11-09 17:41:59 -0300

    concluir desafio 3 node

--- src/index.js
+++ src/index.js
@@ -61,15 +61,14 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repository = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
   }
+   repository.likes = repository.likes + 1;
 
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json(likes);
+  return response.json(repository);
 });
 
 module.exports = app;
